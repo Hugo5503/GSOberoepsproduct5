bc8073ee22850e86108414ad74c78b9d64fafa58
metalfiredemon <ruud.hagens@hotmail.com>
2016-01-05 14:09:05 +0100

    bank tests

--- BankierenNoObserverFX/test/bank/bankieren/BankTest.java
+++ BankierenNoObserverFX/test/bank/bankieren/BankTest.java
@@ -5,6 +5,7 @@
  */
 package bank.bankieren;
 
+import fontys.util.NumberDoesntExistException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,6 +18,11 @@ import static org.junit.Assert.*;
  * @author Ruud
  */
 public class BankTest {
+    Bank b;
+    Klant k1;
+    Klant k2;
+    Rekening r1;
+    Rekening r2;
     
     public BankTest() {
     }
@@ -31,6 +37,13 @@ public class BankTest {
     
     @Before
     public void setUp() {
+        b = new Bank("Rabobank");
+        k1 = new Klant("Hans", "Eindhoven");
+        k2 = new Klant("Hugo", "Veldhoven");
+        r1 = new Rekening(100000000, k1, new Money(100, "€"));
+        r2 = new Rekening(100000001, k2, new Money(100, "€"));
+        b.openRekening("Hans", "Eindhoven");
+        b.openRekening("Hugo", "Veldhoven");
     }
     
     @After
@@ -44,13 +57,20 @@ public class BankTest {
     public void testOpenRekening() {
         System.out.println("openRekening");
         String name = "";
-        String city = "";
-        Bank instance = null;
-        int expResult = 0;
+        String city = "Eindhoven";
+        Bank instance = b;
+        int expResult = -1;
         int result = instance.openRekening(name, city);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals("Rekening is aangemaakt zonder naam", expResult, result);
+        name = "hans";
+        city = "";
+        result = instance.openRekening(name, city);
+        assertEquals("Rekening is aangemaakt zonder stad", expResult, result);
+        name = "Ruud";
+        city = "Bakel";
+        expResult = 100000002;
+        result = instance.openRekening(name, city);
+        assertEquals("Rekening is niet aangemaakt", expResult, result);        
     }
 
     /**
@@ -60,12 +80,10 @@ public class BankTest {
     public void testGetRekening() {
         System.out.println("getRekening");
         int nr = 0;
-        Bank instance = null;
+        Bank instance = b;
         IRekening expResult = null;
         IRekening result = instance.getRekening(nr);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals("Niet bestaande rekening is opgehaald", expResult, result);
     }
 
     /**
@@ -74,15 +92,37 @@ public class BankTest {
     @Test
     public void testMaakOver() throws Exception {
         System.out.println("maakOver");
-        int source = 0;
-        int destination = 0;
-        Money money = null;
-        Bank instance = null;
-        boolean expResult = false;
+        int source = 100000000;
+        int destination = 100000001;
+        Money money = new Money(50, "€");
+        Bank instance = b;
+        boolean expResult = true;
         boolean result = instance.maakOver(source, destination, money);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals("Bedrag is niet overgemaakt", expResult, result);
+        money = new Money(100000, "€");
+        expResult = false;
+        result = instance.maakOver(source, destination, money);
+        assertEquals("Bedrag met overschreeden saldo is overgemaakt" + r1.getSaldo().getCents() + r2.getSaldo().getCents(), expResult, result);
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void testMaakOverDubbelesource() throws NumberDoesntExistException {
+        b.maakOver(100000000, 100000000, new Money(200, "€"));
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void testMaakOverNegatiefBedrag() throws NumberDoesntExistException {
+        b.maakOver(100000000, 100000001, new Money(-200, "€"));
+    }
+    
+    @Test(expected = NumberDoesntExistException.class)
+    public void testMaakOverNonExcistSource() throws NumberDoesntExistException {
+        b.maakOver(1, 100000001, new Money(200, "€"));
+    }
+    
+    @Test(expected = NumberDoesntExistException.class)
+    public void testMaakOverNonExcistDest() throws NumberDoesntExistException {
+        b.maakOver(100000000, 1, new Money(200, "€"));
     }
 
     /**
@@ -91,12 +131,10 @@ public class BankTest {
     @Test
     public void testGetName() {
         System.out.println("getName");
-        Bank instance = null;
-        String expResult = "";
+        Bank instance = b;
+        String expResult = "Rabobank";
         String result = instance.getName();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals("Naam van bank is niet juist opgehaald", expResult, result);
     }
     
 }
